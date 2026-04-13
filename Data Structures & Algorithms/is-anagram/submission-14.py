81750119dc9ff58532a8fe772f287279fb6094d1
Leslie Yodushima <123102343+leslieyodyod@users.noreply.github.com>
2026-04-13 18:57:10 -0400

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashMap = {}
+        s = list(s)
+
+        for i in range(len(s)):
+            if s[i] not in hashMap:
+                hashMap[s[i]] = 1
+            else:
+                hashMap[s[i]] += 1
+        
+        for char in t:
+            if char in hashMap:
+                hashMap[char] -= 1
+            else:
+                return False
+        
+        for value in hashMap.values():
+            if value != 0:
+                return False 
+        return True
+
+        
+        
+             
