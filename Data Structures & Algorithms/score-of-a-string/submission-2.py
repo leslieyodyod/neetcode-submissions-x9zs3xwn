43f5ae73e44e10f373c13f68d051f22a05ddbcb8
Leslie Yodushima <123102343+leslieyodyod@users.noreply.github.com>
2026-06-14 17:40:52 -0400

    Add: score-of-a-string - submission-2

---
+++ Data Structures & Algorithms/score-of-a-string/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        #create a dictionary mapping asci values to letters in s 
+        #iterate through s with left and right pointers
+        #calculate the difference, then keep moving through s
+        #each difference gets added to res 
+        #return res 
+        l, r = 0, 1
+        res = 0
+        while l < r and r < len(s):
+            res += abs(ord(s[l]) - ord(s[r]))
+            l += 1
+            r += 1
+        return res 
