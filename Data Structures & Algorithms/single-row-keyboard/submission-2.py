4f22ebe9ea1eb06a6af05e2c06225a2b2af4cbf8
Leslie Yodushima <123102343+leslieyodyod@users.noreply.github.com>
2026-06-21 15:33:34 -0400

    Add: single-row-keyboard - submission-2

---
+++ Data Structures & Algorithms/single-row-keyboard/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        res = abs(0 - keyboard.index(word[0]))
+
+        for i in range(len(word) - 1):
+            res += abs(keyboard.index(word[i]) - keyboard.index(word[i + 1]))
+        return res
