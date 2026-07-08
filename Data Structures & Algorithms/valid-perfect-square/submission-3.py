807f8eb11edbab66345e0bab95e5e70dc300f072
Leslie Yodushima <123102343+leslieyodyod@users.noreply.github.com>
2026-07-08 17:39:50 -0400

    Add: valid-perfect-square - submission-3

---
+++ Data Structures & Algorithms/valid-perfect-square/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        perf = {}
+
+        for n in range(2**16):
+            perf[n] = n * n
+
+        l, r = 0, len(perf) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if perf[mid] > num:
+                r = mid - 1 
+            elif perf[mid] < num:
+                l = mid + 1
+            else:
+                return True
+            print(mid)
+        return False
+
