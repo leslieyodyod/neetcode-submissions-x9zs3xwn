0c56bb8df85db3aa658fc02e5398cf7bc05b98d4
Leslie Yodushima <123102343+leslieyodyod@users.noreply.github.com>
2026-07-19 16:03:39 -0400

    Add: valid-word-square - submission-6

---
+++ Data Structures & Algorithms/valid-word-square/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def validWordSquare(self, words: List[str]) -> bool:
+
+        for word_num in range(len(words)):
+            for char_pos in range(len(words[word_num])):
+                if char_pos >= len(words) or word_num >= len(words[char_pos]):
+                    return False
+                elif words[word_num][char_pos] != words[char_pos][word_num]:
+                    return False 
+        return True         
+        
+                
+
+        
+        
+          
+
+        
+
+
+        
+        
