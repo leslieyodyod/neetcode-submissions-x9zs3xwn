4d9e7d4d8a5026e3b2912da686db9a23e17a3de5
Leslie Yodushima <123102343+leslieyodyod@users.noreply.github.com>
2026-07-20 19:43:57 -0400

    Add: find-missing-and-repeated-values - submission-1

---
+++ Data Structures & Algorithms/find-missing-and-repeated-values/submission-1.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution:
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        freqMap, maxVal, res = defaultdict(), len(grid[0]) ** 2, [0, 0]
+
+        for i in range(1, maxVal + 1):
+            freqMap[i] = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                freqMap[grid[r][c]] += 1
+        
+        for num, count in freqMap.items():
+            if count > 1:
+                res[0] = num
+            elif count == 0:
+                res[1] = num
+        
+        return res
+            
+
+
+        
+
+
+            
